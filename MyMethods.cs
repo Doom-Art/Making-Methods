ded0d4bca6f5e22d7a0f8f35b5ed92f3be2c2b6d
Doom-Art <ouginak121@gmail.com>
2022-10-11 09:37:16 -0400

    class ended

--- MyMethods.cs
+++ MyMethods.cs
@@ -8,9 +8,17 @@ namespace Making_Methods
 {
     static internal class MyMethods
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public static double GetHypotenuse(double a, double b)
         {
+            double hypot = 0;
 
+            return hypot;
         }
     }
 }
