fcfeba381c8f89482a18b4b0546552355553c92a
Doom-Art <ouginak121@gmail.com>
2022-10-11 09:36:17 -0400

    Add project files.

---
+++ MyMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Making_Methods
+{
+    static internal class MyMethods
+    {
+        public static double GetHypotenuse(double a, double b)
+        {
+
+        }
+    }
+}
